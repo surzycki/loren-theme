dad602dc869fb09b22e9d901d57e72e234a2df6a
stefan <stefan.surzycki@gmail.com>
2021-02-12 20:12:56 +0100

    finish part I

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -3,7 +3,7 @@ import "unfetch/polyfill";
 import example from "./components/example";
 import * as cart from "@shopify/theme-cart";
 
-window.Cart = cart;
+//window.Cart = cart;
 
 // cart.addItem(6164917190854).then((item) => {
 //   console.log(
@@ -12,27 +12,27 @@ window.Cart = cart;
 //   );
 //});
 
-let formData = {
-  items: [
-    {
-      id: 38199853908166,
-      quantity: 2,
-    },
-  ],
-};
+// let formData = {
+//   items: [
+//     {
+//       id: 38199853908166,
+//       quantity: 2,
+//     },
+//   ],
+// };
 
-fetch("/cart/add.js", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(formData),
-})
-  .then((response) => {
-    return response.json();
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+// fetch("/cart/add.js", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// })
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .catch((error) => {
+//     console.error("Error:", error);
+//   });
 
-example();
+// example();
