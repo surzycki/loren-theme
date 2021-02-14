71f953261c1ecdb700661458d14491f38967474e
Stefan Surzycki <stefan.surzycki@gmail.com>
2021-02-14 20:08:25 +0100

    [ADD] React integration (#1)

---
+++ src/scripts/react-cart.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom";
+import React from "react";
+import Cart from "./components/Cart";
+
+const root = document.getElementById("react-cart");
+const checkoutRoute = root.dataset.checkoutRoute;
+
+root && ReactDOM.render(<Cart route={checkoutRoute} />, root);
